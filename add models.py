331092812ec2b1ab61e4d40bca8b7beee4b2e5e7
Ajayab05 <53139108+Ajayab05@users.noreply.github.com>
2026-03-08 13:11:42 +0530

    Update add models.py

--- add models.py
+++ add models.py
@@ -1,5 +1,18 @@
-user = User(
-    username=request.form['username'],
-    email=request.form['email'],
-    password=request.form['password']
-)
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(120))
+
+
+class Game(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    genre = db.Column(db.String(100))
